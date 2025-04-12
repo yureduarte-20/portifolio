9b69fff946152069a34cfba65bffe2e6401591c1
Yure Samarone Gomes Duarte <yuresamarone34@gmail.com>
2025-04-12 18:00:34 -0300

    fix: alpine como modelo de dados

--- src/genExpiriencies.js
+++ src/genExpiriencies.js
@@ -4,6 +4,9 @@ document.addEventListener('alpine:init', () => {
     window.Alpine.data('contentProfile', () => ({
         expiriences: expiriences.expiriencies,
         aboutMe: expiriences.aboutMe,
-        projects: expiriences.projects
+        projects: expiriences.projects,
+        resume: expiriences.resume,
+        currentJobName: expiriences.currentJobName,
+        fullName: expiriences.fullName        
     }))
 })
